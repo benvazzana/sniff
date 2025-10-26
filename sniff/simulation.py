fab4fc03bbc74e29c9a7dfe3707cf607b9da88c7
Ian Snider <iqsnider@gmail.com>
2025-10-26 15:30:31 -0700

    impr: implemented more robust noise calls

--- sniff/simulation.py
+++ sniff/simulation.py
@@ -6,19 +6,55 @@ from scipy.integrate import solve_ivp
 
 
 class Simulate:
-    def __init__(self, A, time, time_step, x0, noise_update_rate=0.1):
+    def __init__(self, A, time, time_step, x0,
+                 noise_model='piecewise', noise_update_rate=0.1):
+        """
+        The noise model options:
+        'static' :  single GOE sample for fixed uncertain channels
+        'piecewise' : update GOE sample at noise_update_rate intervals
+        'white' : update GOE sample at every time step
+        """
         self.T = time
         self.dT = time_step
         self.x0 = x0
         self.rmo = RMT(A)
+        self.noise_model = noise_model
         self.noise_update_rate = noise_update_rate
         self.current_L_noisy = None
         self.last_noise_update = -np.inf
+
+    def _get_noisy_laplacian(self, t, noise_strength):
+        """
+        Returns the proper noisy laplacian based on the simulation
+        specifications
+        """
+        if self.noise_model == 'white':
+            return self.rmo.noise_GOE(noise_strength)
+
+        elif self.noise_model == 'static':
+            if self.current_L_noisy is None:
+                self.current_L_noisy = self.rmo.noise_GOE(noise_strength)
+            return self.current_L_noisy
+
+        elif self.noise_model == 'piecewise':
+            if self.current_L_noisy is None:
+                self.current_L_noisy = self.rmo.noise_GOE(noise_strength)
+                self.last_noise_update = t
+            elif t - self.last_noise_update >= self.noise_update_rate:
+                self.current_L_noisy = self.rmo.noise_GOE(noise_strength)
+                self.last_noise_update = t
+            return self.current_L_noisy
+
+        else:
+            raise ValueError(f"Unknown noise_model: {self.noise_model}")
 
     def solve_consensus_dynamics_w_GOE_noise(self, noise_strength=0.1):
         """
         Solves the consensus dynamics
         """
+        self.current_L_noisy = None
+        self.last_noise_update = -np.inf
+
         t_span = (0, self.T)
         t_eval = np.linspace(*t_span, int(self.T/self.dT))
 
@@ -32,16 +68,17 @@ class Simulate:
         """
         Consensus dynamics state-space model
         """
-        if t - self.last_noise_update >= self.noise_update_rate:
-            self.current_L_noisy = self.rmo.noise_GOE(noise_strength)
-            self.last_noise_update = t
-        return -self.current_L_noisy @ x
+        L_noisy = self._get_noisy_laplacian(t, noise_strength)
+        return -L_noisy @ x
 
     def solve_consensus_setpoint_tracking_w_GOE_noise(self, alpha=1, beta=1, p_track=[0, 0], noise_strength=0.1):
         """
         Solves the consensus dynamics for converging to a 2D
         setpoint
         """
+        self.current_L_noisy = None
+        self.last_noise_update = -np.inf
+
         t_span = (0, self.T)
         t_eval = np.linspace(*t_span, int(self.T/self.dT))
 
@@ -55,7 +92,7 @@ class Simulate:
         return solution
 
     def _consensus_2D_setpoint_w_GOE_noise(self, t, x, alpha, beta, p_track, noise_strength):
-        L_noisy = self.rmo.noise_GOE(noise_strength)
+        L_noisy = self._get_noisy_laplacian(t, noise_strength)
         B, c = Graph.make_setpoint_transform_2D(
             L_noisy, alpha=alpha, beta=beta, p_track=p_track)
         return B @ x + c
@@ -65,6 +102,9 @@ class Simulate:
         Solves the consensus dynamics for converging to a 2D
         setpoint and arranges agents into a formation
         """
+        self.current_L_noisy = None
+        self.last_noise_update = -np.inf
+
         t_span = (0, self.T)
         t_eval = np.linspace(*t_span, int(self.T/self.dT))
 
@@ -78,7 +118,7 @@ class Simulate:
         return solution
 
     def _consensus_formation_2D_setpoint_w_GOE_noise(self, t, x, alpha, beta, p_track, noise_strength, spacing):
-        L_noisy = self.rmo.noise_GOE(noise_strength)
+        L_noisy = self._get_noisy_laplacian(t, noise_strength)
         formation = Simulate.generate_formation(L_noisy.shape[0], spacing)
         B, c = Graph.make_setpoint_formation_transform_2D(
             L_noisy, alpha=alpha, beta=beta, p_track=p_track, formation_offsets=formation)
